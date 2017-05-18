dacf99664a3535bf9704a0cdfac202341575d1a3
Julian <julian@artloftasia.com>
2017-05-18 11:42:23 +0800

    change compare page javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -59,14 +59,13 @@ jQuery(function () {
             if ($(".input-select:checked").length > 4) {
                 $('.compare-button').on('click', function () {
                     alert("You can select up to 4 to compare");
+                    $('.compare-button').attr("disabled", "disabled");
                     return false;
                 });
             }
 
             if ($(".input-select:checked").length >= 2) {
                 $('.compare-button').removeAttr("disabled");
-            } else if ($(".input-select:checked").length > 4) {
-                $('.compare-button').attr("disabled", "disabled");
             } else {
                 $('.compare-button').attr("disabled", "disabled");
             }
