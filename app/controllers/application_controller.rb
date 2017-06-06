be69744334d9b9d8f0e56f262e74ba71a7afc8cb
Noopur <noopijain@gmail.com>
2017-06-06 11:47:19 +0800

    link to home after signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_action :create_hr_role
   protect_from_forgery with: :exception
+
   def after_sign_in_path_for(resource)
     if resource.has_role?(:hruser)
-      root_path
+      '/main'
     else
       '/admin/products/feature_groups'
     end
