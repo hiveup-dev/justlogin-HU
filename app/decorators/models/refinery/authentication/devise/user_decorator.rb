966d5070ea7ee5f8ce933ebb0a36c3b4a0f0f95b
Noopur <noopijain@gmail.com>
2017-06-08 17:02:14 +0800

    correct typo

--- app/decorators/models/refinery/authentication/devise/user_decorator.rb
+++ app/decorators/models/refinery/authentication/devise/user_decorator.rb
@@ -1,4 +1,4 @@
 Refinery::Authentication::Devise::User.class_eval do
   has_many :policies, class_name: 'Refeinery::Policies::Policy'
-  has_many :employess, class_name: 'Refinery::Policies::Employee'
+  has_many :employees, class_name: 'Refinery::Policies::Employee'
 end
