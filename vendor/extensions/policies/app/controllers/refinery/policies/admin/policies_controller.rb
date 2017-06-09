e3ef893915edb7ec6d9333877c34c33e1915abb7
Noopur <noopijain@gmail.com>
2017-06-09 15:54:58 +0800

    allow adding polcies for multipleemployees

--- vendor/extensions/policies/app/controllers/refinery/policies/admin/policies_controller.rb
+++ vendor/extensions/policies/app/controllers/refinery/policies/admin/policies_controller.rb
@@ -19,13 +19,28 @@ module Refinery
           end
         end
 
+        def create_many
+          employee_ids = multiple_employee_params[:employee_id]
+          employee_ids.each do |id|
+            if !id.blank?
+              policy = Policy.new policy_params
+              policy.employee_id = id
+              policy.save
+            end
+          end
+          redirect_to '/admin/policies'
+        end
+
         private
         # Only allow a trusted parameter "white list" through.
         def policy_params
-          params.require(:policy).permit(:name, :logo_id, :insurer, :premium, :benefit_id, :employee_id, :user_id, :position,
+          params.require(:policy).permit(:name, :logo_id, :insurer, :premium, :benefit_id, :user_id, :position,
                                          policy_feature_policies_attributes: [:id, :policy_feature_id,
                                                 :value, :is_in_summary,
                                                 :_destroy])
+        end
+        def multiple_employee_params
+          params.require(:policy).permit(:employee_id => [])
         end
       end
     end
