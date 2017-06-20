27cb2e786e704a6d12acb353c6d62e607f2ebc1a
Noopur <noopijain@gmail.com>
2017-06-20 15:18:40 +0530

    link to profile page

--- app/decorators/controllers/refinery/authentication/devise/users_controller_decorator.rb
+++ app/decorators/controllers/refinery/authentication/devise/users_controller_decorator.rb
@@ -2,13 +2,11 @@ Refinery::Authentication::Devise::UsersController.class_eval do
   before_action :set_user, only: [:edit, :update]
 
   def edit
-    print 'inside edit!!'
   end
 
   def update
     @user.update user_params
     @user.save
-    print 'hello'
     redirect_to '/main', notice: 'Updated successfuly!!'
   end
 
@@ -19,7 +17,7 @@ Refinery::Authentication::Devise::UsersController.class_eval do
   end
 
   def user_params
-    params.require(:user).permit(:username, :email, :full_name, :phone)
+    params.require(:user).permit(:username, :email, :full_name, :phone, :company)
   end
 
 end
