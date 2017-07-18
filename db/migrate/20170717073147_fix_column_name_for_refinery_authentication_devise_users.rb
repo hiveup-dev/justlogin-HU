2bf2bffc7b7d8d008b5d380a99a4c70b92319aa7
jiayingy <jiayinglek@gmail.com>
2017-07-18 14:35:33 +0800

    Remove duplicated migration

--- db/migrate/20170717073147_fix_column_name_for_refinery_authentication_devise_users.rb
+++
@@ -1,5 +0,0 @@
-class FixColumnNameForRefineryAuthenticationDeviseUsers < ActiveRecord::Migration
-  def change
-    rename_column :refinery_authentication_devise_users, :comapany, :company
-  end
-end
