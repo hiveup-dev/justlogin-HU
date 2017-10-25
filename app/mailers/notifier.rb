b1879bf5f506bbc0d1cba839c6ea88e500ec8b80
Julian <julian@artloftasia.com>
2017-10-25 11:28:29 +0800

    change email recipient 'ivan@hive-up.com' to 'policy@hive-up.com'

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,11 +3,11 @@ class Notifier < ApplicationMailer
   def notify_email(submission)
     @submission = submission
     puts 'inside email'
-    mail(to: 'ivan@hive-up.com', subject: '[Notification] New User Submission', from: 'admin@hive-up.com')
+    mail(to: 'policy@hive-up.com', subject: '[Notification] New User Submission', from: 'admin@hive-up.com')
   end
 
   def notify_signup(details)
     @details = details
-    mail(to: 'ivan@hive-up.com', subject: '[SIGN UP] New User Sign Up', from: 'admin@hive-up.com')
+    mail(to: 'policy@hive-up.com', subject: '[SIGN UP] New User Sign Up', from: 'admin@hive-up.com')
   end
 end
